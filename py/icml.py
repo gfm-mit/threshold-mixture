d37e81175768b8a8a1c3e80eb2ae89aa897740b8
gfm-mit <126123509+gfm-mit@users.noreply.github.com>
2024-12-16 10:27:50 -0500

    update downloader

--- py/icml.py
+++ py/icml.py
@@ -3,17 +3,25 @@ import pprint
 import anthropic
 import requests
 
-with open('llm/icml_poster.json', 'r') as f:
+with open('./data/icml_2024_poster.json', 'r') as f:
     data = json.load(f)['notes']
-    data = [d['content'] for d in data]
 
 tpl_url = "https://openreview.net/{}"
 
-pdf_url = tpl_url.format(data[0]['pdf']['value'])
-response = requests.get(pdf_url)
+def process(icml_dict):
+  icml_dict = {
+    k: v['value']
+    for k, v in icml_dict['content'].items()
+  }
+  pdf_url = tpl_url.format(icml_dict['pdf'])
+  response = requests.get(pdf_url)
+  filename = "pdfs/{}.pdf".format(
+    icml_dict['paperhash'].replace('|', '_')
+  )
+  with open(filename, 'wb') as f:
+      f.write(response.content)
 
-with open('downloaded.pdf', 'wb') as f:
-    f.write(response.content)
+process(data[0])
 
 with open('llm/anthropic.key', 'r') as f:
     key = f.read()
