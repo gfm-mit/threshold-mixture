a845e8065a51bb94e6c7b692a00e893131407580
gfm-mit <126123509+gfm-mit@users.noreply.github.com>
2024-12-16 16:23:16 -0500

    Update pandas.py

--- py/pandas.py
+++ py/pandas.py
@@ -16,17 +16,20 @@ for f in glob.glob('summaries/*.txt'):
     except json.JSONDecodeError as e:
       print(f, e)
       continue
+    ff = re.sub("_", " ", re.findall("/([^_]+_[^_]+)", f)[0])
     try:
       data = {
         v['name']: v['present']
         for v in data
       }
-      results[f] = data
+      results[ff] = data
     except TypeError as e:
       print(e)
 results = pd.DataFrame(results)
 idx = results.sum(axis=1).sort_values(ascending=False).index
 results = results.loc[idx]
+idx = results.sum(axis=0).sort_values(ascending=False).index
+results = results.loc[:, idx]
 sns.heatmap(results)
 plt.tight_layout()
 plt.show()
