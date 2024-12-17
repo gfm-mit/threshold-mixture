bf4a7d794fbfefaeb75ba628fab4c7b1686935a3
gfm-mit <126123509+gfm-mit@users.noreply.github.com>
2024-12-17 10:50:05 -0500

    Update icml.py

--- py/icml.py
+++ py/icml.py
@@ -16,22 +16,26 @@ def process(icml_dict, overwrite=False):
   }
   tpl_url = "https://openreview.net/{}"
   pdf_url = tpl_url.format(icml_dict['pdf'])
-  filename = "pdfs/{}.pdf".format(
+  raw_filename = "raw_pdfs/{}.pdf".format(
     icml_dict['paperhash'].replace('|', '_')
   )
-  if os.path.exists(filename) and not overwrite:
+  stripped_filename = "stripped_pdfs/{}.pdf".format(
+    icml_dict['paperhash'].replace('|', '_')
+  )
+  if os.path.exists(stripped_filename) and not overwrite:
     print("PDF already exists")
-    return filename
+    return stripped_filename
   response = requests.get(pdf_url)
-  with open(filename, 'wb') as f:
+  with open(raw_filename, 'wb') as f:
       f.write(response.content)
-  py.pdf.clip_and_compress_pdf(filename, filename, max_pages=8, image_max_size=(100, 100), image_quality=10)
-  return filename
+  py.pdf.clip_and_compress_pdf(raw_filename, stripped_filename, max_pages=8, image_max_size=(100, 100), image_quality=10)
+  return stripped_filename
 
 # Example usage
 if __name__ == "__main__":
     with open('./data/icml_2024_poster.json', 'r') as f:
         data = json.load(f)['notes']
+    assert False, (len(data))
 
     with open('local/anthropic.key', 'r') as f:
         api_key = f.read()
@@ -39,7 +43,7 @@ if __name__ == "__main__":
     for icml_dict in tqdm.cli.tqdm(data):
       paperhash = icml_dict['content']['paperhash']['value']
       pdf_path = process(icml_dict, overwrite=False)
-      txt_path = pdf_path.replace('.pdf', '.txt').replace('pdfs/', 'summaries/')
+      txt_path = pdf_path.replace('.pdf', '.txt').replace('stripped_pdfs/', 'summaries/')
       #if os.path.exists(txt_path):
       #  print('file already exists')
       #else:
