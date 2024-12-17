d1152b34b58e173be85c9cf3a1f74316d2543ad2
gfm-mit <126123509+gfm-mit@users.noreply.github.com>
2024-12-17 10:53:09 -0500

    Create openreview.py

---
+++ py/openreview.py
@@ -0,0 +1,43 @@
+import json
+import os
+import os
+import pprint
+import requests
+import time
+import tqdm
+import py.pdf
+import py.anthropic
+
+import argparse
+
+def scrape_urls(kind, count=2275):
+    assert kind in ["Poster", "Oral", "Spotlight"]
+    tpl = """https://api2.openreview.net/notes?content.venue=ICML%202024%20{}&details=replyCount,presentation&domain=ICML.cc/2024/Conference&invitation=ICML.cc/2024/Conference/-/Submission&limit=400&offset={}"""
+
+    notes = []
+    for k in range(0, count, 400):
+            # Start of Selection
+            url = tpl.format(kind, k)
+            response = requests.get(url)
+            json_data = response.json()
+            notes += json_data['notes']
+
+    return dict(notes=notes, count=len(notes))
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Pretty-print a JSON file.')
+    parser.add_argument('--filename', type=str, help='Path to the JSON file to pretty-print.')
+    parser.add_argument('--kind', type=str, help='Used in the OpenReview scraper')
+    args = parser.parse_args()
+    assert os.path.exists(args.filename)
+
+    if args.kind:
+      data = scrape_urls(args.kind)
+    else:
+      with open(args.filename, 'r') as f:
+          data = json.load(f)
+
+    print(f"Count: {data['count']}")
+    data_str = json.dumps(data, indent=2)
+    with open(args.filename, 'w') as f:
+        f.write(data_str)
