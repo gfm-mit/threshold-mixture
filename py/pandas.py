f0bae2786661af1762d7ba139571e4db39361321
gfm-mit <126123509+gfm-mit@users.noreply.github.com>
2024-12-28 15:45:40 -0500

    Update pandas.py

--- py/pandas.py
+++ py/pandas.py
@@ -12,11 +12,17 @@ for f in glob.glob('summaries/*.txt'):
     data = file.read()
     data = re.sub(r'.*</metric_evaluation>\n*', '', data, flags=re.DOTALL)
     try:
+      data = re.findall('\[\s+\{.*?\}\s+\]', data, flags=re.DOTALL)[0]
+    except IndexError as e:
+      print(e)
+      continue
+    try:
       data = json.loads(data)
     except json.JSONDecodeError as e:
       print(f, e)
       continue
-    ff = re.sub("_", " ", re.findall("/([^_]+_[^_]+)", f)[0])
+    ff = re.findall("/([a-z0-9_]+)", f)[0].split('_')
+    ff = " ".join(ff[2:4])
     try:
       data = {
         v['name']: v['present']
@@ -25,7 +31,8 @@ for f in glob.glob('summaries/*.txt'):
       results[ff] = data
     except TypeError as e:
       print(e)
-results = pd.DataFrame(results)
+results = pd.DataFrame(results).fillna(False)
+print(results)
 idx = results.sum(axis=1).sort_values(ascending=False).index
 results = results.loc[idx]
 idx = results.sum(axis=0).sort_values(ascending=False).index
